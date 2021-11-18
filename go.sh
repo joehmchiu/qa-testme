88a723f4dd3f164eab4cd6a6cf667c0b9b7ac6f0
JoeC <you@example.com>
2021-11-19 10:21:48 +1100

    test me

--- go.sh
+++ go.sh
@@ -2,9 +2,11 @@
 
 env=${1:-"delme"}
 reg=${2:-"uk"}
+abs=$(realpath $0)
+dir=$(dirname $abs)
 
 az account set --subscription "Non-Production Subscription"
-
+cd $dir
 perl 1.create-rg-vnet-snet.pl
 perl 2.create-rg-mgmt-vnet-snet.pl
 perl 3.private-dns-link.pl
