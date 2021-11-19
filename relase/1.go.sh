c91d2ec937c4a49800a31ad6a215e36d9ce1f201
JoeC <you@example.com>
2021-11-19 18:48:46 +1100

    test me

--- relase/1.go.sh
+++ relase/1.go.sh
@@ -6,6 +6,9 @@ abs=$(realpath $0)
 dir=$(dirname $abs)
 
 cd $dir/../
+$dir/../bin/drawit.sh hello
+$dir/../bin/drawit.sh azp
+
 az account set --subscription "Non-Production Subscription"
 perl 1.create-rg-vnet-snet.pl $env $reg
 
--- relase/5.go.sh
+++ relase/5.go.sh
@@ -5,6 +5,9 @@ reg=${2:-"uk"}
 abs=$(realpath $0)
 dir=$(dirname $abs)
 
+$dir/../bin/drawit.sh friday
+$dir/../bin/drawit.sh peanut
+
 cd $dir/../
 
 az account set --subscription "Non-Production Subscription"
